bb8dc893cfd4a84389009ac78ac405261cd597f6
Karev Pavel <karev3@yandex.ru>
2016-11-13 21:31:12 +0300

    fix connection status

--- public_html/assets/js/components/connection/updateConnection.js
+++ public_html/assets/js/components/connection/updateConnection.js
@@ -1,27 +1,28 @@
 const updateConnection = (data) => {
   var svgStatus = '';
+  const keysName = 'dashboard-keys';
 
   if (data) {
     if (data.connection_type === 'remote') {
       $('#settings-item-change-password').hide();
       if (data.box_link === 'up') {
-        svgStatus = '<svg class="dashboard-keys__svg"><use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="sprite.svg#box-success-remote-up"></use></svg>';
+        svgStatus = `${keysName}_remote-up`;
       } else {
-        svgStatus = '<svg class="dashboard-keys__svg"><use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="sprite.svg#box-success-remote-down"></use></svg>';
+        svgStatus = `${keysName}_remote-down`;
       }
     } else if (data.connection_type === 'local') {
       $('#settings-item-change-password').show();
       if (data.box_link === 'up') {
-        svgStatus = '<svg class="dashboard-keys__svg"><use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="sprite.svg#box-success-local-up"></use></svg>';
+        svgStatus = `${keysName}_local-up`;
       } else {
-        svgStatus = '<svg class="dashboard-keys__svg"><use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="sprite.svg#box-success-local-down"></use></svg>';
+        svgStatus = `${keysName}_local-down`;
       }
     }
   } else {
-    svgStatus = '<svg class="dashboard-keys__svg"><use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="sprite.svg#box-fail"></use></svg>';
+    svgStatus = `${keysName}_fail`;
   }
 
-  $('.dashboard-keys').empty().append(svgStatus);
+  $('.dashboard-keys').removeClass(`${keysName}_fail ${keysName}_local-down ${keysName}_local-up ${keysName}_remote-down ${keysName}_remote-up`).addClass(svgStatus);
 };
 
 export default updateConnection;
