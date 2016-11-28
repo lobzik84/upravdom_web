995d7a647f036acf245bdb33ff4fc9794afc67db
lobzik <lobzik@lobzik-SATELLITE-R830>
2016-11-28 16:27:29 +0300

    passw update fix

--- public_html/assets/js/components/settings/settingsEvents.js
+++ public_html/assets/js/components/settings/settingsEvents.js
@@ -6,154 +6,147 @@ import commonData from '../common/commonData'; // global config!
 import ToggleText from '../auth/toggleText';
 
 class SettingsEvents {
-  constructor($target, signName) {
-    this.toggleButtonProfile = new ToggleText($('.settings__save.settings__save_profile'));
-    this.toggleButtonPassword = new ToggleText($('.settings__save.settings__save_password'));
+    constructor($target, signName) {
 
-    if ($target[0].type === 'checkbox') {
-      $target.on('change', () => {
-        SettingsEvents.send();
-      });
-    } else if ($target[0].tagName === 'FORM') {
-      $target.on('submit', (event) => {
-        event.preventDefault();
-        const currentTarget = event.currentTarget;
-        if (currentTarget.id === 'changePassword') {
-          this.replacePassword(currentTarget);
+
+        if ($target[0].type === 'checkbox') {
+            $target.on('change', () => {
+                SettingsEvents.send();
+            });
+        } else if ($target[0].tagName === 'FORM') {
+            $target.on('submit', (event) => {
+                event.preventDefault();
+                const currentTarget = event.currentTarget;
+                if (currentTarget.id === 'changePassword') {
+                    this.replacePassword(currentTarget);
+                } else {
+                    this.submitForm(currentTarget);
+                }
+            });
         } else {
-          this.submitForm(currentTarget);
+            $target.on('click', (event) => {
+                const sign = signName === 'plus' ? '+' : '-';
+                SettingsEvents.switchMinMax(event.currentTarget, sign);
+            }).on('click', debounce(() => {
+                SettingsEvents.send();
+            }, 300));
         }
-      });
-    } else {
-      $target.on('click', (event) => {
-        const sign = signName === 'plus' ? '+' : '-';
-        SettingsEvents.switchMinMax(event.currentTarget, sign);
-      }).on('click', debounce(() => {
-        SettingsEvents.send();
-      }, 300));
-    }
-  }
-
-  submitForm(currentTarget) {
-    const $this = $(currentTarget);
-    const $submitButton = $this.find('.settings__save');
-
-    const $email = $this.find('.settings__input_email');
-
-    if ($email.val().length === 0 && !commonData.email.test($email.val())) {
-      this.toggleButtonProfile.toggle('Введите электронную почту');
-      return false;
     }
 
-    if (DEBUG) {
-      console.log('replace submitForm');
-    }
+    submitForm(currentTarget) {
 
-    SettingsEvents.send(() => {
-      $this.attr('disabled', false);
-      $this.find('input').attr('readonly', false);
-      $submitButton.text('Сохранить').removeClass('settings__save_success');
-    });
-    $submitButton.text('Сохраняем').addClass('settings__save_success');
-    $this.attr('disabled', true);
-    $this.find('input').attr('readonly', true);
-  }
 
-  replacePassword(currentTarget) {
-    const $this = $(currentTarget);
-    const $submitButton = $this.find('.settings__save');
+        const $this = $(currentTarget);
+        const $submitButton = $this.find('#settings_save_profile');
+        const toggleButtonProfile = new ToggleText($submitButton);
+        const $email = $this.find('.settings__input_email');
+        toggleButtonProfile.toggle('Сохраняем');
+        if ($email.val().length === 0 && !commonData.email.test($email.val())) {
+            this.toggleButtonProfile.toggle('Введите электронную почту');
+            return false;
+        }
 
-    SettingsEvents.send(() => {
-      if (DEBUG) {
-        console.log('replace password');
-      }
-      if (commonData.connection_type === 'local') {
-        const newPassword = $this.find('.settings__input_new').val();
-        const newLogin = $this.find('.settings__input_phone').val();
-        const oldPass = $this.find('.settings__input_current').val();
-        const confirmPassword = $this.find('.settings__input_confirm').val();
-        if (confirmPassword === newPassword && newPassword.length > 0) {
-          const success = () => {
-            if (DEBUG) {
-              console.log('successfully updated password');
-            }
+        if (DEBUG) {
+            console.log('replace submitForm');
+        }
+
+        SettingsEvents.send(() => {
             $this.attr('disabled', false);
             $this.find('input').attr('readonly', false);
-            $submitButton.text('Сохранено').removeClass('settings__save_success');
-          }
-          const error = () => {
-            if (DEBUG) {
-              console.log('SRP error while updating password');
-            }
-            $this.attr('disabled', false);
-            $this.find('input').attr('readonly', false);
-
-            this.toggleButtonPassword.toggle('Ошибка');
             $submitButton.removeClass('settings__save_success');
-          }
-          updateSRP(oldPass, newLogin, newPassword, success, error);
-        } else {
-          this.toggleButtonPassword.toggle('Пароли не совпадают');
-          $this.attr('disabled', false);
-          $this.find('input').attr('readonly', false);
-          $submitButton.text('Сохранить').removeClass('settings__save_success');
+        });
+        toggleButtonProfile.toggle('Сохранено');
+    }
+
+    replacePassword(currentTarget) {
+        const $this = $(currentTarget);
+        const $submitButton = $this.find('#settings_save_password');
+        const toggleButtonPassword = new ToggleText($submitButton);
+
+        SettingsEvents.send(() => {
+            if (DEBUG) {
+                console.log('replace password');
+            }
+            const newPassword = $this.find('.settings__input_new').val();
+            if (!SettingsEvents.validatePassword(newPassword)) {
+                toggleButtonPassword.toggle('Слишком короткий пароль');
+            }else if (commonData.connection_type === 'local') {
+                toggleButtonPassword.toggle('Сохраняем');
+                const newLogin = $this.find('.settings__input_phone').val();
+                const oldPass = $this.find('.settings__input_current').val();
+                const confirmPassword = $this.find('.settings__input_confirm').val();
+                if (confirmPassword === newPassword && newPassword.length > 0) {
+                    const success = () => {
+                        if (DEBUG) {
+                            console.log('successfully updated password');
+                        }
+                        toggleButtonPassword.toggle('Сохранено');
+                    }
+                    const error = () => {
+                        if (DEBUG) {
+                            console.log('SRP error while updating password');
+                        }
+                        toggleButtonPassword.toggle('Ошибка');
+
+                    }
+                    updateSRP(oldPass, newLogin, newPassword, success, error);
+                } else {
+                    toggleButtonPassword.toggle('Пароли не совпадают');
+                }
+            } else {
+                alert(`Смена пароля возможна только при прямом подключении ${commonData.connection_type}`);
+            }
+        });
+
+    }
+    
+    static validatePassword(newPassword) {
+               
+        return (newPassword.length > commonData.passwordlength);
+    }
+
+    static switchMinMax(currentTarget, sign) {
+        const $target = $(currentTarget);
+        const $valueBlock = $target.siblings('.settings-options__value').children('b');
+        const count = $valueBlock[0].id === 'settings__value--VACAlertMin' || $valueBlock[0].id === 'settings__value--VACAlertMax' ? 5 : 1;
+        let value = parseInt($valueBlock.text().replace(/[^-0-9]/gim, ''), 10);
+
+        if (sign === '+') {
+            value += count;
+        } else if (sign === '-') {
+            value -= count;
         }
-      } else {
-        this.toggleButtonPassword.toggle('Смена пароля возможна только при прямом подключении');
-        alert(`Смена пароля возможна только при прямом подключении ${commonData.connection_type}`);
 
-        $this.attr('disabled', false);
-        $this.find('input').attr('readonly', false);
-        $submitButton.val('Сохранить').removeClass('settings__save_success');
-      }
-    });
-    $submitButton.val('Сохраняем').addClass('settings__save_success');
-    $this.attr('disabled', true);
-    $this.find('input').attr('readonly', true);
-  }
-
-  static switchMinMax(currentTarget, sign) {
-    const $target = $(currentTarget);
-    const $valueBlock = $target.siblings('.settings-options__value').children('b');
-    const count = $valueBlock[0].id === 'settings__value--VACAlertMin' || $valueBlock[0].id === 'settings__value--VACAlertMax' ? 5 : 1;
-    let value = parseInt($valueBlock.text().replace(/[^-0-9]/gim, ''), 10);
-
-    if (sign === '+') {
-      value += count;
-    } else if (sign === '-') {
-      value -= count;
+        $valueBlock.text(value);
+        if (DEBUG) {
+            console.log('Saving settings');
+        }
     }
 
-    $valueBlock.text(value);
-    if (DEBUG) {
-      console.log('Saving settings');
+    static send(callback) {
+        const settingElementIdPrefix = 'settings__value--';
+        const settings = {};
+        const $settings = $(`[id^="${settingElementIdPrefix}"]`);
+
+        $settings.each((index, setting) => {
+            const settingName = setting.id.replace(settingElementIdPrefix, '');
+
+            if (setting.type === 'checkbox') {
+                settings[settingName] = `${setting.checked}`;
+            } else if (setting.type === 'text' || setting.type === 'email') {
+                settings[settingName] = setting.value;
+            } else {
+                settings[settingName] = setting.innerHTML;
+            }
+            updateNameSettings(settingName, settings[settingName]);
+            if (DEBUG) {
+                console.log(`${settingName} - ${setting.innerHTML}`);
+            }
+        });
+        sendCommand('save_settings', {
+            settings
+        }, typeof callback === 'function' ? callback : null);
     }
-  }
-
-  static send(callback) {
-    const settingElementIdPrefix = 'settings__value--';
-    const settings = {};
-    const $settings = $(`[id^="${settingElementIdPrefix}"]`);
-
-    $settings.each((index, setting) => {
-      const settingName = setting.id.replace(settingElementIdPrefix, '');
-
-      if (setting.type === 'checkbox') {
-        settings[settingName] = `${setting.checked}`;
-      } else if (setting.type === 'text' || setting.type === 'email') {
-        settings[settingName] = setting.value;
-      } else {
-        settings[settingName] = setting.innerHTML;
-      }
-      updateNameSettings(settingName, settings[settingName]);
-      if (DEBUG) {
-        console.log(`${settingName} - ${setting.innerHTML}`);
-      }
-    });
-    sendCommand('save_settings', {
-      settings
-    }, typeof callback === 'function' ? callback : null);
-  }
 }
 
 export default SettingsEvents;
